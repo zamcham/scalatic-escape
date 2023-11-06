17ae14c4eb7d5a5158e08e587c176b1dd4fd21f7
Cristian Zambrano <criszamcham@gmail.com>
2023-11-05 17:14:06 -0700

    Add run method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float jumpForce = 10f;
+    [SerializeField] float moveSpeed = 10f;
+    Vector2 moveInput;
     Rigidbody2D rb;
-    bool isOnGround = true;
-    LayerMask groundLayer = 1 << 8;
+
+
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
     }
 }
