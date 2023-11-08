9162d92594ca738a3f77f4d297127f362ac882b0
YeDemirkiran <yedemirkiran27@gmail.com>
2023-11-08 07:34:49 +0300

    Created a general purpose BarUI script
    -The script has a private Slider field. It gets the Slider component in the Start method, and sets its max value to the serialzied private float 'maxValue'.
    
    -It has one public method for setting its value. When this method is called, a coroutine is started internally to lerp the current value to the target value. The duration of this transition is set by serialized private field 'valueLerpDuration'.

---
+++ Assets/BarUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class BarUI : MonoBehaviour
+{
+    Slider slider;
+
+    float currentValue;
+
+    [SerializeField] float maxValue;
+    [SerializeField] float valueLerpDuration = 0.25f;
+
+    Coroutine lerpCoroutine;
+    
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+        slider.maxValue = maxValue;
+    }
+
+    public void SetValue(float value)
+    {
+        currentValue = value;
+        currentValue = Mathf.Clamp(currentValue, 0f, maxValue);
+
+        if (lerpCoroutine != null) { StopCoroutine(lerpCoroutine); }
+        lerpCoroutine = StartCoroutine(ValueLerp(value, valueLerpDuration));
+    }
+
+    IEnumerator ValueLerp(float targetValue, float duration)
+    {
+        float lerp = 0f;
+        float startingValue = slider.value;
+
+        while (lerp < 1f)
+        {
+            lerp += Time.deltaTime / duration;
+
+            slider.value = Mathf.Lerp(startingValue, targetValue, lerp);
+
+            yield return null;
+        }
+    }
+}
