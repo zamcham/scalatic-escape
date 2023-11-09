b5942d799e6e0ee4da756600c83d30ec8f691f8d
Cristian Zambrano <criszamcham@gmail.com>
2023-11-09 10:28:00 -0700

    Add scene vars to game manager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int currentSceneIndex;
+    int totalSceneCount;
+
+    void Awake()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        totalSceneCount = SceneManager.sceneCountInBuildSettings;
+        Debug.Log(totalSceneCount);
+        DontDestroyOnLoad(this.gameObject);
+    }
     void Start()
     {
         
