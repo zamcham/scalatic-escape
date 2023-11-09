6118b4e307fa68e471ea55917c89764009d7314c
Cristian Zambrano <criszamcham@gmail.com>
2023-11-09 10:46:58 -0700

    Add levels dictionary

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -7,22 +7,34 @@ public class GameManager : MonoBehaviour
 {
     public int currentSceneIndex;
     int totalSceneCount;
+    
+    //Dictionary of levels and their completion status
+    public Dictionary<int, bool> levelStatus;
 
     void Awake()
     {
         currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         totalSceneCount = SceneManager.sceneCountInBuildSettings;
-        Debug.Log(totalSceneCount);
         DontDestroyOnLoad(this.gameObject);
     }
     void Start()
     {
-        
+        levelStatus = new Dictionary<int, bool>
+        {
+            {1, true},
+            {2, false},
+            {3, false}
+        };
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadLevelsMap()
+    {
+        SceneManager.LoadScene("Levels Map");
     }
 }
