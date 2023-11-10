c899dce223c4da2813616a69e1fe4bcde374d526
Cristian Zambrano <criszamcham@gmail.com>
2023-11-10 12:22:20 -0700

    Organize player controller script

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,7 +10,6 @@ public class PlayerController : MonoBehaviour
 
     [Header("Jumping")]
     BoxCollider2D groundChecker;
-
     [SerializeField] float jumpInterval = 0.25f;
     int maxJumpCount = 1;
     int jumpCount = 0, jumpsInQueue = 0;
@@ -22,9 +21,6 @@ public class PlayerController : MonoBehaviour
     [Header("Size Shifting & Energy")]
     EnergyManagement energyManagement;
     GameObject pixie, nomad, titan;
-
-
-    // Titan-Specific
     bool canBreakPlatform = false;
     Renderer titanRenderer;
 
@@ -32,18 +28,13 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         energyManagement = GetComponent<EnergyManagement>();
-
         GetGroundChecker();
         GetCharacters();        
     }
 
     void Start()
     {
-        // Reset characters
-        nomad.SetActive(false);
-        titan.SetActive(false);
-        pixie.SetActive(false);
-
+        ResetCharacters();
         OnNomad();
     }
 
@@ -51,13 +42,13 @@ public class PlayerController : MonoBehaviour
     {
         Run();
         FlipSprite();
-
         CheckGround();
         Jump();
         CheckBottomBoundary();
     }
 
     #region Movement
+
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
@@ -70,52 +61,39 @@ public class PlayerController : MonoBehaviour
 
     void FlipSprite()
     {
-        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
-        if (playerHasHorizontalSpeed)
-        {
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Epsilon)
             transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
-        }        
     }
 
     void OnJump()
     {
-        // Queue can only be 1 larger than maxJumpCount
-        // This is to prevent infinite queues
         if (jumpsInQueue < maxJumpCount)
-        {
             jumpsInQueue++;
-        }
     }
 
     void Jump()
     {
-        if (jumpsInQueue > 0)
+        if (jumpsInQueue > 0 && jumpTimer <= 0f)
         {
-            if (jumpTimer <= 0f)
+            if (jumpCount < maxJumpCount) 
             {
-                if (jumpCount < maxJumpCount) 
-                {
-                    jumpTimer = jumpInterval;
-                    jumpsInQueue--;
-                    jumpCount++;
-
-                    rb.velocity = new Vector2(rb.velocity.y, jumpForce);
-                }           
+                jumpTimer = jumpInterval;
+                jumpsInQueue--;
+                jumpCount++;
+                rb.velocity = new Vector2(rb.velocity.y, jumpForce);
             }
-            else
-            {
-                jumpTimer -= Time.deltaTime;
-            }
+        }
+        else
+        {
+            jumpTimer -= Time.deltaTime;
         }
     }
 
-
     bool IsGrounded()
     {
-        bool grounded = groundChecker.IsTouchingLayers(LayerMask.GetMask("Jumpable")) && rb.velocity.y <= 0f;
-
-        return grounded;
+        return groundChecker.IsTouchingLayers(LayerMask.GetMask("Jumpable")) && rb.velocity.y <= 0f;
     }
+
     void CheckGround()
     {
         if (IsGrounded())
@@ -127,72 +105,53 @@ public class PlayerController : MonoBehaviour
 
     void GroundPounding(Collision2D collision)
     {
-        if (canBreakPlatform)
+        if (canBreakPlatform && collision.gameObject.CompareTag("Breakable"))
+            Destroy(collision.gameObject);
+        else if (canBreakPlatform)
         {
-            if (collision.gameObject.CompareTag("Breakable"))
-            {
-                Destroy(collision.gameObject);
-            }
-            else
-            {
-                canBreakPlatform = false;
-                titanRenderer.sharedMaterial.color = Color.white;
-            }
+            canBreakPlatform = false;
+            titanRenderer.sharedMaterial.color = Color.white;
         }
     }
 
     void GetGroundChecker()
     {
         Transform groundCheckerTransform = transform.Find("GroundChecker");
-
-        if (groundCheckerTransform == null)
-        {
-            Debug.LogError("GroundChecker not found!");
-        }
+        if (groundCheckerTransform != null)
+            groundChecker = groundCheckerTransform.GetComponent<BoxCollider2D>();
         else
-        {
-            groundChecker = groundCheckerTransform.gameObject.GetComponent<BoxCollider2D>();
-        }
+            Debug.LogError("GroundChecker not found!");
     }
-
 
     void CheckBottomBoundary()
     {
         if (groundChecker.IsTouchingLayers(LayerMask.GetMask("BottomBoundary"))) 
-        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
     }
+
     #endregion
 
     #region Size Shifting
 
     void OnTitan()
     {
-        if (!titan.activeSelf)
+        if (!titan.activeSelf && energyManagement.currentEnergy >= energyManagement.maxEnergy * (energyManagement.titanEnergyThreshold / 100f))
         {
-            if (energyManagement.currentEnergy >= energyManagement.maxEnergy * (energyManagement.titanEnergyThreshold / 100f))
+            energyManagement.AddEnergyPercent(-energyManagement.titanEnergyCost);
+            pixie.SetActive(false);
+            nomad.SetActive(false);
+            titan.SetActive(true);
+
+            maxJumpCount = 0;
+
+            if (jumpCount > 0)
             {
-                energyManagement.AddEnergyPercent(-energyManagement.titanEnergyCost);
-
-                pixie.SetActive(false);
-                nomad.SetActive(false);
-
-                titan.SetActive(true);
-
-                maxJumpCount = 0;
-
-                // Check for ground pounding
-                if (jumpCount > 0)
-                {
-                    canBreakPlatform = true;
-
-                    titanRenderer.sharedMaterial.color = Color.red;
-                }
-                else
-                {
-                    titanRenderer.sharedMaterial.color = Color.white;
-                }
+                canBreakPlatform = true;
+                titanRenderer.sharedMaterial.color = Color.red;
+            }
+            else
+            {
+                titanRenderer.sharedMaterial.color = Color.white;
             }
         }
     }
@@ -202,31 +161,22 @@ public class PlayerController : MonoBehaviour
         if (!nomad.activeSelf)
         {
             energyManagement.AddEnergyPercent(-energyManagement.nomadEnergyCost);
-
             pixie.SetActive(false);
             titan.SetActive(false);
-
             nomad.SetActive(true);
-
             maxJumpCount = 1;
         }
     }
 
     void OnPixie()
     {       
-        if (!pixie.activeSelf)
+        if (!pixie.activeSelf && energyManagement.currentEnergy >= energyManagement.maxEnergy * (energyManagement.pixieEnergyThreshold / 100f))
         {
-            if (energyManagement.currentEnergy >= energyManagement.maxEnergy * (energyManagement.pixieEnergyThreshold / 100f))
-            {
-                energyManagement.AddEnergyPercent(-energyManagement.pixieEnergyCost);
-
-                titan.SetActive(false);
-                nomad.SetActive(false);
-
-                pixie.SetActive(true);
-
-                maxJumpCount = 2;
-            }
+            energyManagement.AddEnergyPercent(-energyManagement.pixieEnergyCost);
+            titan.SetActive(false);
+            nomad.SetActive(false);
+            pixie.SetActive(true);
+            maxJumpCount = 2;
         }
     }
 
@@ -239,26 +189,26 @@ public class PlayerController : MonoBehaviour
             Transform currentForm = transform.Find(form);
 
             if (currentForm == null)
-            {
                 Debug.LogError($"{form} mesh not found!");
-            }
             else
-            {
-                switch (form)
-                {
-                    case "Pixie":
-                        this.pixie = currentForm.gameObject;
-                        break;
-                    case "Nomad":
-                        this.nomad = currentForm.gameObject;
-                        break;
-                    case "Titan":
-                        this.titan = currentForm.gameObject;
+                AssignCharacter(form, currentForm.gameObject);
+        }
+    }
 
-                        titanRenderer = titan.transform.Find("Body").GetComponent<Renderer>();
-                        break;
-                }
-            }
+    void AssignCharacter(string form, GameObject character)
+    {
+        switch (form)
+        {
+            case "Pixie":
+                pixie = character;
+                break;
+            case "Nomad":
+                nomad = character;
+                break;
+            case "Titan":
+                titan = character;
+                titanRenderer = titan.transform.Find("Body").GetComponent<Renderer>();
+                break;
         }
     }
 
@@ -276,5 +226,12 @@ public class PlayerController : MonoBehaviour
             ICollectible collectible = collision.gameObject.GetComponent<ICollectible>();
             collectible.OnCollect(gameObject);
         }
+    }
+
+    void ResetCharacters()
+    {
+        nomad.SetActive(false);
+        titan.SetActive(false);
+        pixie.SetActive(false);
     }
 }
