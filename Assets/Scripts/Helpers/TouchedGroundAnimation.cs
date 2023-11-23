7bfe06807e6ce0c7da88df27838829250ae51baa
Cristian Zambrano <criszamcham@gmail.com>
2023-11-22 22:15:39 -0700

    Add helper for jump animation

---
+++ Assets/Scripts/Helpers/TouchedGroundAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchedGroundAnimation : MonoBehaviour
+{
+    PlayerAnimations playerAnimations;
+
+    void  Awake()
+    {
+        playerAnimations = FindObjectOfType<PlayerAnimations>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.GetComponent<BoxCollider2D>().IsTouchingLayers(LayerMask.GetMask("Jumpable")))
+        {
+            Debug.Log("Touched Ground");
+            playerAnimations.StartAnimation("Idle", true, 1f);
+        }
+    }
+
+}
